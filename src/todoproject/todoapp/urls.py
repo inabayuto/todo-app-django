ce2d39c268e6ebca06f7e5a4ce3a53ddb9b0f1ef
稲葉勇人 <74127544+inabayuto@users.noreply.github.com>
2025-06-02 11:58:21 +0900

    タスクの追加

--- src/todoproject/todoapp/urls.py
+++ src/todoproject/todoapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', views.TaskList.as_view()),
-    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task')
+    path('', views.TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
+    path('create-task/', views.TaskCreate.as_view(), name='create-task')
 ]
--- src/todoproject/todoapp/views.py
+++ src/todoproject/todoapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Task
+from django.urls import reverse_lazy
 
 # Create your views here.
 class TaskList(ListView):
@@ -11,3 +12,8 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('tasks') # 成功したらtasksにリダイレクト
